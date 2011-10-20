4299388322c30c3770c632dead13a8af465e6470
Kurry Tran <kurry.tran@gmail.com>
2011-10-20 00:13:35 -0400

    Initial Commit

---
+++ StoryboardTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardTutorial
+//
+//  Created by Kurry Tran on 10/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
